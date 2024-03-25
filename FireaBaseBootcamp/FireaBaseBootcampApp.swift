1390823406b414a54b95dee33fb09445e8282960
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-25 11:17:00 +0100

    Initial Commit

---
+++ FireaBaseBootcamp/FireaBaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  FireaBaseBootcampApp.swift
+//  FireaBaseBootcamp
+//
+//  Created by Maya Ghamloush on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FireaBaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
